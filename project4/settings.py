4e7c9b6fc02f9448b9263190e3558e8a4bcb7b8a
ROBI HIDAYAT <robihidayat354@gmail.com>
2022-12-25 00:52:25 +0700

    Update settings.py

--- project4/settings.py
+++ project4/settings.py
@@ -123,5 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'network/media')
 MEDIA_URL = '/media/'
